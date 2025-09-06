3a7b3d80091b21747aad90e7bad8482b43d43e08
abhishek853 <abhiwakhade853@example.com>
2025-09-06 20:32:46 +0530

    Swagger API configuration

---
+++ doctor/backend/physiobook_doctor/src/main/java/com/physiobook/physiobook_doctor/controller/DoctorController.java
@@ -0,0 +1,48 @@
+package com.physiobook.physiobook_doctor.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*
+
+we can access our Swagger API by this URL
+
+http://localhost:8080/swagger-ui/index.html#/
+
+ */
+
+
+@RestController
+@RequestMapping("/api")
+@Tag(name = "Doctor APIs", description = "APIs for doctor operations")  //this is for our class level api information having name and description
+public class DoctorController {
+
+    @GetMapping("/hello")            // this is for our individual api endpoint
+    @Operation(summary = "Say Hello", description = "Returns a greeting message") // that name and description
+    public String hello() {
+        return "Hello, Physiobook!";
+    }
+
+    @PostMapping("/validate")
+    @Operation(summary = "Validate user", description = "Checks if username and password are valid")
+    public ResponseEntity<Map<String, String>> validateUser(
+            @RequestParam String username,
+            @RequestParam String password) {
+
+        Map<String, String> response = new HashMap<>();
+        if ("harish".equals(username) && "1234".equals(password)) {
+            response.put("message", "User is valid");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            response.put("message", "Invalid username or password");
+            return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
